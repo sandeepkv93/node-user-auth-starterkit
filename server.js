2309fc1bc7772bba7ee15cee6461366dc5111f73
Sandeep Vishnu <sandeepkv93@gmail.com>
2020-08-10 11:52:59 -0700

    graceful shutdown of the node server

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var app = require('./app');
 var debug = require('debug')('doctor-helper:server');
 var http = require('http');
-const { connectToDB } = require('./config/db');
+const { connectToDB, disconnectFromDB } = require('./config/db');
+const { Console } = require('console');
 
 const normalizePort = (val) => {
     var port = parseInt(val, 10);
@@ -48,6 +49,25 @@ const onListening = () => {
     console.log('Server is listening at port: ' + addr.port);
 };
 
+const handleShutdown = (signal) => {
+    console.log('got %s, starting shutdown', signal);
+    if (!server.listening) {
+        process.exit(0);
+    }
+    disconnectFromDB()
+        .then(() => {
+            console.log("Disconnected from Mongoose");
+            server.close(err => {
+                if (err) {
+                    console.error(err)
+                    return process.exit(1)
+                }
+                console.log('exiting')
+                process.exit(0)
+            })
+        });
+}
+
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
@@ -60,6 +80,7 @@ connectToDB()
         server.on('listening', onListening);
     });
 
-
-
-
+// Graceful shutdowns
+process.on('SIGINT', handleShutdown);
+process.on('SIGTERM', handleShutdown);
+process.on('SIGHUP', handleShutdown);
